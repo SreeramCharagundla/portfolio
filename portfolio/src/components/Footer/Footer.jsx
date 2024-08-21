55ca8e38b77bb7009a83d94c64171b5b6127e55a
SreeramCharagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2024-08-21 13:33:30 -0400

    updated address

--- portfolio/src/components/Footer/Footer.jsx
+++ portfolio/src/components/Footer/Footer.jsx
@@ -119,7 +119,7 @@ const Footer = () => {
 
         <div className={css.right}>
           <div className={css.info}>
-            <span className="secondaryText">Information</span>
+            <span className="secondaryText">Address</span>
             <p>29 Lambert st, Boston, MA 02119, USA</p>
           </div>
           <ul className={css.menu}>
