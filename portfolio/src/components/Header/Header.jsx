a54743fae42f873f3e4e7dd0d29971534898adaf
Sreeram Charagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2025-02-16 15:25:42 -0500

    resume

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
           <li><a href="#work">Experience</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
           <li>
-  <a href="https://drive.google.com/file/d/1BxBbS1w0Q9KrFQ24n8aee1B1fsHO6A16/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
+  <a href="https://drive.google.com/file/d/17GdtHO0gyt0R7d-sdBmVPQMibpcRZBWe/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
 </li>
 
           {/* <li className={`flexCenter ${css.phone}`}>
